47bc357ad445e97889457e86ca09b7812535241a
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-07-24 00:02:33 -0400

    ESTdate

--- src/components/date.js
+++ src/components/date.js
@@ -1,11 +1,15 @@
-import { format } from 'date-fns';
+import { format, utcToZonedTime } from 'date-fns-tz';
+
+const timeZone = 'America/New_York'; // EST time zone
 
 export function getCurrentFormattedDate() {
   const now = new Date();
-  return format(now, 'MMM dd yyyy - hh:mm a');
+  const estDate = utcToZonedTime(now, timeZone);
+  return format(estDate, 'MMM dd yyyy - hh:mm a');
 }
 
 export function getCurrentDate() {
   const now = new Date();
-  return format(now, 'yyyy-MM-dd');
+  const estDate = utcToZonedTime(now, timeZone);
+  return format(estDate, 'yyyy-MM-dd');
 }
