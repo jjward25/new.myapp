81155af93225a58edfcf9eb97a00f3f1cd1bec5b
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-07-25 12:57:54 -0400

    cachecontrolv2

--- src/app/api/routines/recent/route.js
+++ src/app/api/routines/recent/route.js
@@ -1,24 +1,24 @@
-// src/app/api/routines/recent/route.js
+// src/app/api/routines/recent.js
 
 import { getMostRecentRoutine } from '../../../../utils/mongoDB/routinesCRUD';
 
-// Forcing dynamic behavior by setting cache control headers
-const cacheControlHeader = {
-  'Cache-Control': 'no-cache, no-store, must-revalidate',
-};
-
+// Example of Cache-Control header configuration
 export async function GET(req) {
   try {
     const routine = await getMostRecentRoutine();
     return new Response(JSON.stringify(routine), {
       status: 200,
-      headers: cacheControlHeader,
+      headers: {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+      },
     });
   } catch (error) {
     console.error('Error fetching most recent routine:', error);
     return new Response(JSON.stringify({ error: 'Unable to fetch most recent routine' }), {
       status: 500,
-      headers: cacheControlHeader,
+      headers: {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+      },
     });
   }
 }
