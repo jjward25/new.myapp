b417bef8fffb92682ed06aaf6c7894ae9c718e88
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-07-25 12:24:48 -0400

    forceDynamic

--- src/app/api/routines/recent/route.js
+++ src/app/api/routines/recent/route.js
@@ -1,16 +1,24 @@
-// src/app/api/routines/recent.js
+// src/app/api/routines/recent/route.js
 
 import { getMostRecentRoutine } from '../../../../utils/mongoDB/routinesCRUD';
+
+// Forcing dynamic behavior by setting cache control headers
+const cacheControlHeader = {
+  'Cache-Control': 'no-cache, no-store, must-revalidate',
+};
 
 export async function GET(req) {
   try {
     const routine = await getMostRecentRoutine();
-    return new Response(JSON.stringify(routine), { 
+    return new Response(JSON.stringify(routine), {
       status: 200,
-      headers: { 'Cache-Control': 'no-cache, no-store, must-revalidate' } // Disable caching
+      headers: cacheControlHeader,
     });
   } catch (error) {
     console.error('Error fetching most recent routine:', error);
-    return new Response(JSON.stringify({ error: 'Unable to fetch most recent routine' }), { status: 500 });
+    return new Response(JSON.stringify({ error: 'Unable to fetch most recent routine' }), {
+      status: 500,
+      headers: cacheControlHeader,
+    });
   }
 }
