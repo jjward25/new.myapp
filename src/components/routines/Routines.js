5dd6a943b5471ae667423a4c19cf4d042be54bae
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-07-25 14:34:03 -0400

    hoverStyle

--- src/components/routines/Routines.js
+++ src/components/routines/Routines.js
@@ -15,7 +15,7 @@ const Routines = () => {
   return (
     <div className='w-full'>
       <div className="cursor-pointer flex items-center justify-between p-2 bg-gradient-to-br from-purple-500 via-cyan-500 to-pink-600 rounded-lg mb-3" onClick={toggleOpen}>
-      <p className={`${isOpen ? 'text-cyan-200' : 'text-neutral-800'} 'text-xl md:text-3xl font-semibold`}>
+      <p className={`${isOpen ? 'text-cyan-200' : 'text-neutral-800'} hover:text-cyan-200 text-xl md:text-3xl font-semibold`}>
       {`Daily Check-In`}
         </p>
         <svg
--- src/components/tasks/ClientWrappers/ListClientWrapper.js
+++ src/components/tasks/ClientWrappers/ListClientWrapper.js
@@ -32,7 +32,7 @@ const ListClientWrapper = () => {
   return (
     <div className='flex flex-col w-full justify-start'>
       <div className="cursor-pointer flex items-center justify-between p-2 bg-gradient-to-br from-purple-500 via-cyan-500 to-pink-600 rounded-lg mb-3" onClick={toggleOpen}>
-      <p className={`${isOpen ? 'text-cyan-200' : 'text-neutral-800'} 'text-xl md:text-3xl font-semibold`}>
+      <p className={`${isOpen ? 'text-cyan-200' : 'text-neutral-800'} hover:text-cyan-200 text-xl md:text-3xl font-semibold`}>
         Quick Lists
         </p>
         <svg
