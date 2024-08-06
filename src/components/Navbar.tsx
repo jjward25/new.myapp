0741e293b45c8f09bc51b833f1ecbb7d89943165
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-08-05 21:42:47 -0400

    navReOrder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,8 @@ export default function Navbar() {
                     <div className="absolute -inset-3  bg-gradient-to-tr from-cyan-300 via-neutral-300 to-cyan-300 dark:bg-black blur opacity-90"></div>
                     <div className="relative rounded-bl-lg rounded-br-lg md:rounded-lg flex justify-around border-2 border-cyan-200">
                         <Link className="hover:scale-95 font-semibold text-cyan-700 dark:text-cyan-900 hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-br from-purple-500 via-cyan-500 to-pink-600 text-lg" href={"/"}>Home</Link>
-                        <Link className="hover:scale-95 font-semibold text-cyan-700 dark:text-cyan-900 hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-br from-purple-500 via-cyan-500 to-pink-600 text-lg" href={"/backlog"}>Backlog</Link>
                         <Link className="hover:scale-95 font-semibold text-cyan-700 dark:text-cyan-900 hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-br from-purple-500 via-cyan-500 to-pink-600 text-lg" href={"/projects"}>Projects</Link>
+                        <Link className="hover:scale-95 font-semibold text-cyan-700 dark:text-cyan-900 hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-br from-purple-500 via-cyan-500 to-pink-600 text-lg" href={"/backlog"}>Backlog</Link>
                     </div>
                 </div>
             </nav>
